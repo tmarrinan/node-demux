33b2f946dd2cdac61dd167db0e4758692f51ea19
Thomas Marrinan <thomas.j.marrinan@gmail.com>
2014-12-30 11:18:44 -0600

    added example

---
+++ example/example.js
@@ -0,0 +1,32 @@
+var fs    = require('fs');
+var demux = require('../../node-demux');
+
+
+var start;
+var nframes;
+var format;
+
+var video = new demux();
+video.on('error', function(err) {
+    console.log(err);
+});
+video.on('metadata', function(metadata) {
+    nframes = metadata.num_frames;
+    format = metadata.pixel_format;
+    console.log(metadata);
+});
+video.on('start', function() {
+    start = Date.now();
+    console.log("start demuxing");
+});
+video.on('end', function() {
+    var time = (Date.now() - start) / 1000;
+    console.log("finished demuxing");
+    console.log("  total time: " + time.toFixed(1) + "sec");
+    console.log("  average frames per second: " + (nframes/time).toFixed(1) + "fps");
+});
+video.on('frame', function(frameIdx, data) {
+    console.log("received frame " + frameIdx + " (size: " + data.length + ")"); 
+});
+video.loadVideo("skyfall_1080p.mp4");
+video.startDemuxing();
