caadaad49f8cd05f9c06da00351253f20f590c43
Victor Mateevitsi <mvictoras@gmail.com>
2015-01-29 17:29:34 -0600

    Fixing ubuntu build

--- node-videodemux.h
+++ node-videodemux.h
@@ -5,6 +5,7 @@ extern "C" {
 	#include <libavcodec/avcodec.h>
 	#include <libavformat/avformat.h>
 	#include <libavutil/imgutils.h>
+	#include <libavutil/frame.h>
 }
 #include <string>
 #include <vector>
